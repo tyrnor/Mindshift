386bf6f3ae28ca8ee77044f9169a66c286a20e4f
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-20 14:29:22 +0200

    Add shouldWin state and conditional win/lose prompt

--- Mindshift/ContentView.swift
+++ Mindshift/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var shouldWin = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if shouldWin {
+                Text("You have to win")
+            } else {
+                Text("You have to lose")
+            }
         }
         .padding()
     }
