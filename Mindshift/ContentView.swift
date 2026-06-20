7c13ba0635801817944e8980b7de9bcfa9d7abac
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-20 14:14:01 +0200

    Initial Commit

---
+++ Mindshift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mindshift
+//
+//  Created by Carlos Yanez Puig on 20/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mindshift/MindshiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindshiftApp.swift
+//  Mindshift
+//
+//  Created by Carlos Yanez Puig on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MindshiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
