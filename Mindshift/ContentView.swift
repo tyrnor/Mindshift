b3a74f2490820a5c70b7fe46fee4fc32660c6576
Carlos Yáñez <carlosyanez997@gmail.com>
2026-06-20 15:18:56 +0200

    Add winningMove state and logic to determine which is the winning move

--- Mindshift/ContentView.swift
+++ Mindshift/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var shouldWin = false
     @State private var move = getMove()
+    @State private var winningMove: Move?
     
     var body: some View {
         VStack {
@@ -19,6 +20,24 @@ struct ContentView: View {
                 Text("You have to win")
             } else {
                 Text("You have to lose")
+            }
+            Button("Rock") {
+                winningMove = getWinningMove(move, shouldWin)
+                if winningMove == Move.rock {
+                    print("You win!")
+                }
+            }
+            Button("Scissors") {
+                winningMove = getWinningMove(move, shouldWin)
+                if winningMove == Move.scissors {
+                    print("You win!")
+                }
+            }
+            Button("Paper") {
+                winningMove = getWinningMove(move, shouldWin)
+                if winningMove == Move.paper {
+                    print("You win!")
+                }
             }
         }
         .padding()
@@ -29,6 +48,22 @@ func getMove() -> Move {
     guard let move = Move.allCases.randomElement() else { return Move.paper
     }
     return move
+}
+
+func getWinningMove(_ move: Move, _ shouldWin: Bool) -> Move {
+    if shouldWin {
+        switch move {
+        case .rock: return .paper
+        case .paper: return .scissors
+        case .scissors: return .rock
+        }
+    } else {
+        switch move {
+        case .rock: return .scissors
+        case .paper: return .rock
+        case .scissors: return .paper
+        }
+    }
 }
 
 #Preview {
